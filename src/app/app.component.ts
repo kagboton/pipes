d6b3d5aa7363539173a69052eac266e501bb432d
Kilian Agboton <agbotonstefane@gmail.com>
2024-06-27 11:49:59 -0400

    used some  built in angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
+import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleCasePipe, DatePipe, CurrencyPipe, DecimalPipe, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pipes';
+  payeeName: string = '';
+  paymentDate: string = '';
+  amount: number = 0;
+  height: number = 0;
+
+  car = {
+    make: 'Tesla',
+    model: 'Model S',
+    year: 2022,
+    mileage: 150000,
+    color: 'Black',
+    price: 60000,
+  }
+
+  onNameChange(event: Event) {
+    this.payeeName = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange(event: Event) {
+    this.paymentDate = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = Number((event.target as HTMLInputElement).value);
+  }
+
+  onHeightChange(event: Event) {
+    this.height = Number((event.target as HTMLInputElement).value);
+  }
 }
